2a33eb2aea1b121954d7205313f67f05692ce986
enzenoide <henzefernandes1974@gmail.com>
2026-01-26 20:54:38 -0300

    Implemented all editor commands

--- Editor.cpp
+++ Editor.cpp
@@ -12,10 +12,73 @@ int Editor::getRow() const{
 }
 
 bool Editor::Enter(){
-    if(text -> split_lines(row,col)){
+    if(text -> split_lines(getRow(),getCol())){
       row++;
       col = 0;
       return true;
   }
     return false;
+}
+bool Editor::Backspace(){
+   if(text -> deleter(getRow(),getCol())){
+      col--;
+      return true;
+  }
+   else if (col == 0) {
+      if(int prevLine = text -> lineLength(row-1)){
+          text -> merge_lines(row,col);
+          row--;
+          col += prevLine;
+          return true;
+    }
+  }
+    return false;
+}
+bool Editor::Space(){
+    if(text -> insert(' ',row,col)){
+      col++;
+      return true;
+  }
+    return false;
+}
+bool Editor::Tab(){
+   for(int i = 0; i < 8; ++i){
+      text -> insert(' ',row,col);
+      col++;
+  }
+    return true;
+}
+bool Editor::moveRight(){
+    if(col <= text -> lineLength(row)){
+      col++;
+  }
+    return true;
+}
+bool Editor::moveLeft(){
+    if(col >= 0){
+      col--;
+  }
+    return true;
+}
+bool Editor::moveUp(){
+  int prevLine = text -> lineLength(row-1);
+  int currentLine = text -> lineLength(row);
+  if(currentLine < prevLine){
+      row--;
+      col = prevLine;
+      return true;
+  }
+  row--;
+  return true;
+}
+bool Editor::moveDown(){
+  int nextLine = text -> lineLength(row+1);
+  int currentLine = text -> lineLength(row);
+  if(currentLine > nextLine){
+      row++;
+      col = nextLine;
+      return true;
+  }
+  row++;
+  return true;
 }
