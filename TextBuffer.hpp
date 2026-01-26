2c30f916badf3c19e6b3e1ed13138f6ba6fab3ee
enzenoide <henzefernandes1974@gmail.com>
2026-01-26 20:54:08 -0300

    Implementing new function: lineLength

--- TextBuffer.hpp
+++ TextBuffer.hpp
@@ -4,10 +4,12 @@
 class TextBuffer{
   public:
     bool insert(char character, int rol, int col);
-    bool deleter(char character,int rol, int col);
+    bool deleter(int rol, int col);
     bool split_lines(int row,int col);
     bool merge_lines(int row,int col);
     bool isValid(int row,int col);
+    bool lineLength(int row);
 private:
     std::vector<std::vector<char>> storage;
 };
+
